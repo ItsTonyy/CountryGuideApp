ef6fd98c2a8bbd99cf0752857d7ece84fa71cfb6
Tony <tonytnportela@outlook.com>
2023-04-06 00:23:12 -0300

    Javascript quase completo

--- src/app.js
+++ src/app.js
@@ -1,21 +1,49 @@
 let myBtn = document.querySelector('.search-button');
 let textInput = document.querySelector('.input-text');
+// let result = document.querySelector('.main-box')
 
 const handleError = (error) => {
   console.log('deu ruim paizão', error);
 };
 
 myBtn.addEventListener('click', () => {
-  let countryName = 'Brazil';
+  let countryName = 'Brazil'; // TODO associar essa variável ao input
   let finalUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
   fetch(finalUrl)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      // testes abaixo:
+
+      // console.log(data)
+      // console.log(data[0].flags.png)
+      // console.log(data[0].name.common)
+      // console.log(data[0].capital[0])
+      // console.log(data[0].continents[0])
+      // console.log(data[0].population)
+      // console.log(Object.keys(data[0].currencies)[0])
+      // console.log(data[0].currencies[Object.keys(data[0].currencies)].name)
+      // console.log(Object.values(data[0].languages).toString().split(',').join(', '))
+      
+      result.innerHTML = `
+
+      <div class = 'data-box'>
+      <img src="${data[0].flags.png}" alt="imagem" class = 'flag-img'>
+      <h2 class = 'country-name' >${data[0].name.common}</h2>
+
+      <p>Capital: ${data[0].capital[0]} </p>
+      <p>Continent: ${data[0].continents[0]} </p>
+      <p>Population: ${data[0].population} </p>
+      <p>Currency: ${Object.keys(data[0].currencies)[0] + data[0].currencies[Object.keys(data[0].currencies)].name} </p>
+      <p>Common Languages: ${Object.values(data[0].languages).toString().split(',').join(', ')} </p>
+      
+      </div>
+      `;
     })
     .catch((error) => {
       console.log('deu erro paizão', error);
     });
+
+    
 });
 
 // png: imagem
